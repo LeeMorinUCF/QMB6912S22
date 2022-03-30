aaa898a4b67667764d7e9f9ec0b51d7102c28aa6
Lee Morin <42477710+LeeMorinUCF@users.noreply.github.com>
2022-03-30 08:07:13 -0400

    Models with Box-Tidwell transformation to predict tractor prices

--- demo_09/Tractor_Box_Tidwell/Code/Tractor_Box_Tidwell.R
+++ demo_09/Tractor_Box_Tidwell/Code/Tractor_Box_Tidwell.R
@@ -725,87 +725,212 @@ cat("\\end{verbatim}", file = out_file_name, append = TRUE)
 # Box-Tidwell Transformation
 ##################################################
 
+# The boxTidwell function tests for non-linear relationships
+# to the mean of the dependent variable.
+# The nonlinearity is in the form of an
+# exponential transformation in the form of the Box-Cox
+# transformation, except that the transformation is taken
+# on the explanatory variables.
+
 #--------------------------------------------------
-# First model: transformation of horsepower
+# Transformation of horsepower
 #--------------------------------------------------
 
 # Modified from the linear model:
 # saleprice ~ horsepower + squared_horsepower +
 #   age + enghours +
 #   diesel + fwd + manual + johndeere + cab
+# This specification allows a single exponential
+# transformation on horsepower, rather than a quadratic form.
 
-box_tidwell <- boxTidwell(formula =
-                             saleprice ~ horsepower,
-                          other.x = ~ age + enghours +
-                             diesel + fwd + manual + johndeere + cab,
-                          data = tractor_sales)
+
+bt_hp <- boxTidwell(formula =
+                       log_saleprice ~ horsepower,
+                    other.x = ~
+                       # horsepower + squared_horsepower +
+                       age +
+                       enghours +
+                       diesel + fwd + manual + johndeere + cab,
+                    data = tractor_sales)
 
 # The summary method is not available.
-# summary(box_tidwell)
+# summary(bt_hp)
 
-print(box_tidwell)
+# The output is a test on the exponent.
+print(bt_hp)
 # Note: The "MLE of lambda" is the exponent on horsepower.
-# I mistakenly interpreted this as the exponent on saleprice, like Box-Cox,
-# but with Box-Tidwell, the exponents are on the explanatory variables
+# Similar to the Box-Cox transformation,
+# with Box-Tidwell, the exponents are on the explanatory variables
 # and are all called lambda.
-# The exponent is not significantly different from 1,
-# although it is slightly lower.
+# The exponent is significantly different from 0,
+# although it is a small positive value,
+# which suggests an increasing but sharply declining relationship.
+
+
+# Print the output to a LaTeX file.
+tab_file_name <- 'bt_hp.tex'
+out_file_name <- sprintf('%s/%s', tab_dir, tab_file_name)
+cat("\\begin{verbatim}", file = out_file_name)
+sink(out_file_name, append = TRUE)
+print(bt_hp)
+sink()
+cat("\\end{verbatim}", file = out_file_name, append = TRUE)
 
 
 #--------------------------------------------------
-# Second model: transformation of horsepower and age
+# Transformation of age
 #--------------------------------------------------
 
 
-box_tidwell <- boxTidwell(formula =
-                             saleprice ~ horsepower + age,
-                          other.x = ~ diesel + enghours + fwd + manual + johndeere + cab,
-                          data = tractor_sales)
+bt_age <- boxTidwell(formula =
+                        log_saleprice ~ age,
+                     other.x = ~
+                        horsepower + squared_horsepower +
+                        # age +
+                        enghours +
+                        diesel + fwd + manual + johndeere + cab,
+                     data = tractor_sales)
 
-print(box_tidwell)
-# The exponent  on horsepower is not significantly different from 1,
-# although it is slightly lower.
-# The exponent for age is near -1,
-# which corresponds to the reciprocal:
-# a quick decline in value in the first year of age
-# that declines throughout the life of the tractor.
+print(bt_age)
+# This coefficient is effectively 1, which is more evidence of
+# a purely linear relationship between log_saleprice
+# and age: the percentage depreciation rate is constant.
 
-
-#--------------------------------------------------
-# Third model: Repeat with log saleprice
-#--------------------------------------------------
-
-box_tidwell <- boxTidwell(formula =
-                             log_saleprice ~ horsepower + age,
-                          other.x = ~ enghours +
-                             diesel + fwd + manual + johndeere + cab,
-                          data = tractor_sales)
-
-print(box_tidwell)
-# With the log saleprice as the dependent variable again,
-# we are back to having an exponent on horsepower.
-# Age now has a linear effect and no onger needs a transformation.
-
+# Print the output to a LaTeX file.
+tab_file_name <- 'bt_age.tex'
+out_file_name <- sprintf('%s/%s', tab_dir, tab_file_name)
+cat("\\begin{verbatim}", file = out_file_name)
+sink(out_file_name, append = TRUE)
+print(bt_age)
+sink()
+cat("\\end{verbatim}", file = out_file_name, append = TRUE)
 
 
 #--------------------------------------------------
-# Fourth model: transformation of engine hours and age
-# with horsepower specified with squared horsepower
+# Transformation of engine hours
 #--------------------------------------------------
 
-# Now revert back to squared horsepower.
 
-box_tidwell <- boxTidwell(formula =
-                             log_saleprice ~ enghours + age,
-                          other.x = ~ horsepower + squared_horsepower +
-                             diesel + fwd + manual + johndeere + cab,
-                          data = tractor_sales)
-print(box_tidwell)
-# None of the exponents are significant.
-# The linear model with squared horsepower was sufficient.
+bt_eng <- boxTidwell(formula =
+                        log_saleprice ~ enghours,
+                     other.x = ~
+                        horsepower + squared_horsepower +
+                        age +
+                        # enghours +
+                        diesel + fwd + manual + johndeere + cab,
+                     data = tractor_sales)
+
+print(bt_eng)
+# Although not statistically significant,
+# this suggests a moderately increasing relationship
+# between log_saleprice and engine hours,
+# which means that tractors with high hours of use
+# depreciate more quickly with each additional hour of use.
+
+# Since a nonlinear relationship was detected with horsepower,
+# check with nonlinearity in all three continuous variables.
 
 
+bt_eng <- boxTidwell(formula =
+                        log_saleprice ~ enghours,
+                     other.x = ~
+                        horsepower + squared_horsepower +
+                        age +
+                        # enghours +
+                        diesel + fwd + manual + johndeere + cab,
+                     data = tractor_sales)
 
+print(bt_eng)
+
+
+# Print the output to a LaTeX file.
+tab_file_name <- 'bt_eng.tex'
+out_file_name <- sprintf('%s/%s', tab_dir, tab_file_name)
+cat("\\begin{verbatim}", file = out_file_name)
+sink(out_file_name, append = TRUE)
+print(bt_eng)
+sink()
+cat("\\end{verbatim}", file = out_file_name, append = TRUE)
+
+#--------------------------------------------------
+# Transformation of horsepower, ageand engine hours
+#--------------------------------------------------
+
+bt_full <- boxTidwell(formula =
+                        log_saleprice ~ horsepower +
+                         age +
+                         enghours,
+                     other.x = ~
+                        # horsepower + squared_horsepower +
+                        # age +
+                        # enghours +
+                        diesel + fwd + manual + johndeere + cab,
+                     data = tractor_sales)
+
+print(bt_full)
+
+# This confirms the result of the above,
+# with the only nonlinear transformation
+# for horsepower.
+# This suggests an additional model with
+# this transformation of horsepower.
+
+# Print the output to a LaTeX file.
+tab_file_name <- 'bt_full.tex'
+out_file_name <- sprintf('%s/%s', tab_dir, tab_file_name)
+cat("\\begin{verbatim}", file = out_file_name)
+sink(out_file_name, append = TRUE)
+print(bt_full)
+sink()
+cat("\\end{verbatim}", file = out_file_name, append = TRUE)
+
+
+##################################################
+# Linear specification from the significant
+# exponent in the Box-Tidwell transformation.
+##################################################
+
+print(bt_hp)
+# MLE of lambda Score Statistic (z)     Pr(>|z|)
+#     0.1143693           -7.386372 1.508894e-13
+bt_hp_lambda_hat <- 0.1143693
+
+
+# Create a variable horsepower_bt
+# to investigate nonlinear relationship of log sale price to horsepower.
+tractor_sales[, 'horsepower_bt'] <-
+   tractor_sales[, 'horsepower']^bt_hp_lambda_hat
+
+# Estimate a regression model.
+lm_bt_hp <- lm(data = tractor_sales,
+               formula = log_saleprice ~
+                  horsepower_bt +
+                  age +
+                  enghours +
+                  diesel + fwd + manual + johndeere + cab)
+
+# Output the results to screen.
+print(summary(lm_bt_hp))
+
+# The performance is similar to the other models with
+# forms of nonlinearity for the value of horsepower.
+# Put them in a table for a final comparison.
+
+# Print the output to a LaTeX file.
+tab_file_name <- 'reg_sq_horse_sp_bt.tex'
+out_file_name <- sprintf('%s/%s', tab_dir, tab_file_name)
+texreg(l = list(lm_7,
+                lm_sp_hp_1,
+                lm_bt_hp),
+       digits = 5,
+       file = out_file_name,
+       label = 'tab:reg_sq_horse_sp_bt',
+       caption = "Alternate Models for Tractor Prices")
+
+# The last model has the highest R-squared
+# among the ones we have estimated.
+# The differences are marginal, however, so the practical recommendation
+# is the model with the quadratic relationship for horsepower.
 
 
 ##################################################
